14b028b8ad28c5f5b62ab0fa35525bc6c880c8d4
snow <lz@snow.xyz>
2023-05-12 16:52:58 -0400

    fix tooltip css not loading

--- src/lib/components/shared/BuffTooltip.svelte
+++ src/lib/components/shared/BuffTooltip.svelte
@@ -11,19 +11,3 @@
     <img src={$skillIcon.path + getSkillIcon(synergy.source.icon)} alt={synergy.name} class="w-5 h-5 table-cell"
         use:tooltip={{content: generateHeaderTooltip(synergy, $skillIcon.path)}}/>
 </div>
-<style>
-    :global(.tippy-box[data-theme='buff']) {
-        @apply inline-block px-2 py-1 font-medium rounded-lg shadow-sm text-xs;
-        @apply text-gray-200 bg-zinc-900;
-    }
-    :global(.tippy-box[data-theme='menu']) {
-        @apply inline-block px-2 py-1 text-sm font-medium rounded-lg shadow-sm;
-        @apply text-gray-200 bg-zinc-700;
-    }
-    :global(.tippy-box[data-theme='buff'] > .tippy-svg-arrow) {
-        @apply fill-zinc-900;
-    }
-    :global(.tippy-box[data-theme='menu'] > .tippy-svg-arrow) {
-        @apply fill-zinc-700;
-    }
-</style>
