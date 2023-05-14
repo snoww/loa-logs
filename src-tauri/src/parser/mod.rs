82f89e5e73abd37fc8c7e0e6176b5f543c9b8cbf
snow <lz@snow.xyz>
2023-05-14 00:26:26 -0400

    fix battle items showing as aeromancer skills

--- src-tauri/src/parser/mod.rs
+++ src-tauri/src/parser/mod.rs
@@ -1274,7 +1274,7 @@ fn get_skill_name_and_icon(
 ) -> (String, String) {
     if skill_id == 0 && skill_effect_id == 0 {
         ("Bleed".to_string(), "buff_168.png".to_string())
-    } else if skill_id == 0 {
+    } else if skill_effect_id != 0 {
         if let Some(effect) = SKILL_EFFECT_DATA.get(&skill_effect_id) {
             if effect.item_name.is_some() {
                 return (
