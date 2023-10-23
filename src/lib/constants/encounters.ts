9d4f7c1a8597c6c7db2e6fe6122ec960c01f7b11
snow <lz@snow.xyz>
2023-10-23 14:50:29 -0400

    add some missing bosses to filter
    changed bosses such that they match the hell gates

--- src/lib/constants/encounters.ts
+++ src/lib/constants/encounters.ts
@@ -5,7 +5,8 @@ export const encounterMap: { [key: string]: { [key: string]: Array<string> }} = 
     },
     Vykas: {
         "Vykas G1": ["Incubus Morphe", "Nightmarish Morphe"],
-        "Vykas G2": ["Covetous Devourer Vykas", "Covetous Legion Commander Vykas"]
+        "Vykas G2": ["Covetous Devourer Vykas"],
+        "Vykas G3": ["Covetous Legion Commander Vykas"]
     },
     Clown: {
         "Clown G1": ["Saydon"],
@@ -16,7 +17,9 @@ export const encounterMap: { [key: string]: { [key: string]: Array<string> }} = 
         "Brelshaza G1": ["Gehenna Helkasirs"],
         "Brelshaza G2": ["Prokel", "Prokel's Spiritual Echo", "Ashtarot"],
         "Brelshaza G3": ["Primordial Nightmare"],
-        "Brelshaza G4": ["Phantom Legion Commander Brelshaza"]
+        "Brelshaza G4": ["Phantom Legion Commander Brelshaza"],
+        "Brelshaza G5": ["Brelshaza, Monarch of Nightmares", "Imagined Primordial Nightmare", "Pseudospace Primordial Nightmare"],
+        "Brelshaza G6": ["Phantom Legion Commander Brelshaza"]
     },
     Kayangel: {
         "Kayangel G1": ["Tienis"],
