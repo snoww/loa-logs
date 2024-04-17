45bb30ff89a1974f4fb2afd14f735d117222a7f4
snow <lz@snow.xyz>
2024-04-17 20:46:13 +0900

    hide rdps header columns for 2nd party in live meter

--- src/lib/components/shared/RdpsHeader.svelte
+++ src/lib/components/shared/RdpsHeader.svelte
@@ -4,6 +4,13 @@
 
     export let meterSettings: any;
     export let partyId: number | undefined = undefined;
+    export let isLiveMeter = false;
+
+    let showHeaderForOtherParties = true;
+
+    $: {
+        showHeaderForOtherParties = !(partyId !== undefined && partyId > 0 && isLiveMeter);
+    }
 </script>
 
 <thead>
@@ -34,41 +41,73 @@
             {/if}
             <th class="w-14 px-2 text-left font-normal" />
             <th class="w-full" />
-            <th
-                class="w-14 font-normal"
-                use:tooltip={{ content: "Damage dealt without any synergies + Damage given from your synergies" }}
-                >rDMG
-            </th>
-            <th class="w-14 font-normal" use:tooltip={{ content: "rDamage per second" }}>rDPS</th>
-            <th class="w-14 font-normal" use:tooltip={{ content: "rDamage %" }}>rD%</th>
-            {#if meterSettings.rdpsDamageReceived}
-                <th class="w-14 font-normal" use:tooltip={{ content: "Total damage received from friendly synergies" }}
-                    >Recv</th>
-            {/if}
-            {#if meterSettings.rdpsDamageGiven}
-                <th class="w-14 font-normal" use:tooltip={{ content: "Total damage given with your synergies" }}
-                    >Given</th>
-            {/if}
-            {#if meterSettings.rdpsContribution}
-                <th class="w-14 font-normal" use:tooltip={{ content: "% of your Damage that is from all synergies" }}
-                    >Con%</th>
-            {/if}
-            {#if meterSettings.rdpsSContribution}
-                <th class="w-14 font-normal" use:tooltip={{ content: "% of your Damage that is from the Support" }}
-                    >sCon%</th>
-            {/if}
-            {#if meterSettings.rdpsDContribution}
-                <th class="w-14 font-normal" use:tooltip={{ content: "% of your Damage that is from Dealers" }}
-                    >dCon%</th>
-            {/if}
-            {#if meterSettings.rdpsSyn}
-                <th class="w-14 font-normal" use:tooltip={{ content: "% of Damage gained all synergies" }}>Syn%</th>
-            {/if}
-            {#if meterSettings.rdpsSSyn}
-                <th class="w-14 font-normal" use:tooltip={{ content: "% of Damage gained from Support" }}>sSyn%</th>
-            {/if}
-            {#if meterSettings.rdpsDSyn}
-                <th class="w-14 font-normal" use:tooltip={{ content: "% of Damage gained from Dealers" }}>dSyn%</th>
+            {#if showHeaderForOtherParties}
+                <th
+                    class="w-14 font-normal"
+                    use:tooltip={{ content: "Damage dealt without any synergies + Damage given from your synergies" }}
+                    >rDMG
+                </th>
+                <th class="w-14 font-normal" use:tooltip={{ content: "rDamage per second" }}>rDPS</th>
+                <th class="w-14 font-normal" use:tooltip={{ content: "rDamage %" }}>rD%</th>
+                {#if meterSettings.rdpsDamageReceived}
+                    <th
+                        class="w-14 font-normal"
+                        use:tooltip={{ content: "Total damage received from friendly synergies" }}>Recv</th>
+                {/if}
+                {#if meterSettings.rdpsDamageGiven}
+                    <th class="w-14 font-normal" use:tooltip={{ content: "Total damage given with your synergies" }}
+                        >Given</th>
+                {/if}
+                {#if meterSettings.rdpsContribution}
+                    <th
+                        class="w-14 font-normal"
+                        use:tooltip={{ content: "% of your Damage that is from all synergies" }}>Con%</th>
+                {/if}
+                {#if meterSettings.rdpsSContribution}
+                    <th class="w-14 font-normal" use:tooltip={{ content: "% of your Damage that is from the Support" }}
+                        >sCon%</th>
+                {/if}
+                {#if meterSettings.rdpsDContribution}
+                    <th class="w-14 font-normal" use:tooltip={{ content: "% of your Damage that is from Dealers" }}
+                        >dCon%</th>
+                {/if}
+                {#if meterSettings.rdpsSyn}
+                    <th class="w-14 font-normal" use:tooltip={{ content: "% of Damage gained all synergies" }}>Syn%</th>
+                {/if}
+                {#if meterSettings.rdpsSSyn}
+                    <th class="w-14 font-normal" use:tooltip={{ content: "% of Damage gained from Support" }}>sSyn%</th>
+                {/if}
+                {#if meterSettings.rdpsDSyn}
+                    <th class="w-14 font-normal" use:tooltip={{ content: "% of Damage gained from Dealers" }}>dSyn%</th>
+                {/if}
+            {:else}
+                <th class="w-14 font-normal"> </th>
+                <th class="w-14 font-normal"></th>
+                <th class="w-14 font-normal"></th>
+                {#if meterSettings.rdpsDamageReceived}
+                    <th class="w-14 font-normal"></th>
+                {/if}
+                {#if meterSettings.rdpsDamageGiven}
+                    <th class="w-14 font-normal"></th>
+                {/if}
+                {#if meterSettings.rdpsContribution}
+                    <th class="w-14 font-normal"></th>
+                {/if}
+                {#if meterSettings.rdpsSContribution}
+                    <th class="w-14 font-normal"></th>
+                {/if}
+                {#if meterSettings.rdpsDContribution}
+                    <th class="w-14 font-normal"></th>
+                {/if}
+                {#if meterSettings.rdpsSyn}
+                    <th class="w-14 font-normal"></th>
+                {/if}
+                {#if meterSettings.rdpsSSyn}
+                    <th class="w-14 font-normal"></th>
+                {/if}
+                {#if meterSettings.rdpsDSyn}
+                    <th class="w-14 font-normal"></th>
+                {/if}
             {/if}
         {/if}
     </tr>
