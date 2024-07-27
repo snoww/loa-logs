snoww/loa-logs

commit 2227905ca363fd2b2f0833366337165e09bab571

no debounce if clearing search filter

src/lib/components/table/TableFilter.svelte
@@ -46,6 +46,10 @@
     function debounce(fn: FormEventHandler<HTMLInputElement>, milliseconds: number) {
         let timer: number | undefined;
 
+        if ($searchStore.length === 0) {
+            return fn;
+        }
+
         return (evt: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
             clearTimeout(timer);
             timer = setTimeout(() => {

commit 5079ebb807332bcaf7a629636b070f7a86dc2596

fix: debounce regression on search input
this was introduced in 2227905ca363fd2b2f0833366337165e09bab571

src/lib/components/table/TableFilter.svelte
@@ -46,21 +46,16 @@
     function debounce(fn: FormEventHandler<HTMLInputElement>, milliseconds: number) {
         let timer: number | undefined;
 
-        if ($searchStore.length === 0) {
-            return fn;
-        }
-
         return (evt: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
             clearTimeout(timer);
-            timer = setTimeout(() => {
-                fn(evt);
-            }, milliseconds);
+            const timeout = $searchStore.length ? milliseconds : 0;
+            timer = setTimeout(() => fn(evt), timeout);
         };
     }
 
     const handleSearchInput = debounce((e) => {
         loadEncountersFn();
-    }, 500);
+    }, 300);
 
     const isFilterButton = (element: HTMLElement) => {
         return element.classList.contains("filter-button");