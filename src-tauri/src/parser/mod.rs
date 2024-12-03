0a98f9ae0c037467d66e0733ee7d99b09e8f9313
snow <lz@snow.xyz>
2024-12-03 13:08:25 +0900

    remove heartbeat

--- src-tauri/src/parser/mod.rs
+++ src-tauri/src/parser/mod.rs
@@ -1071,37 +1071,37 @@ pub fn start(window: Window<Wry>, port: u16, settings: Option<Settings>) -> Resu
             party_map_cache = HashMap::new();
         }
 
-        if last_heartbeat.elapsed() >= heartbeat_duration {
-            let client = client.clone();
-            let client_id = client_id.clone();
-            let version = window.app_handle().package_info().version.to_string();
-            let region = match state.region {
-                Some(ref region) => region.clone(),
-                None => continue,
-            };
-            tokio::task::spawn(async move {
-                let request_body = json!({
-                    "id": client_id,
-                    "version": version,
-                    "region": region,
-                });
-
-                match client
-                    .post(format!("{API_URL}/heartbeat"))
-                    .json(&request_body)
-                    .send()
-                    .await
-                {
-                    Ok(_) => {
-                        debug_print(format_args!("sent heartbeat"));
-                    }
-                    Err(e) => {
-                        warn!("failed to send heartbeat: {:?}", e);
-                    }
-                }
-            });
-            last_heartbeat = Instant::now();
-        }
+        // if last_heartbeat.elapsed() >= heartbeat_duration {
+        //     let client = client.clone();
+        //     let client_id = client_id.clone();
+        //     let version = window.app_handle().package_info().version.to_string();
+        //     let region = match state.region {
+        //         Some(ref region) => region.clone(),
+        //         None => continue,
+        //     };
+        //     tokio::task::spawn(async move {
+        //         let request_body = json!({
+        //             "id": client_id,
+        //             "version": version,
+        //             "region": region,
+        //         });
+        // 
+        //         match client
+        //             .post(format!("{API_URL}/heartbeat"))
+        //             .json(&request_body)
+        //             .send()
+        //             .await
+        //         {
+        //             Ok(_) => {
+        //                 debug_print(format_args!("sent heartbeat"));
+        //             }
+        //             Err(e) => {
+        //                 warn!("failed to send heartbeat: {:?}", e);
+        //             }
+        //         }
+        //     });
+        //     last_heartbeat = Instant::now();
+        // }
     }
 
     Ok(())
