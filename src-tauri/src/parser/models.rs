6a208b06f2158f8562896aae831ec82f9b408d59
snow <lz@snow.xyz>
2025-04-18 17:15:29 -0400

    no longer need to maintain settings in rust

--- src-tauri/src/parser/models.rs
+++ src-tauri/src/parser/models.rs
@@ -7,7 +7,7 @@ use bitflags::bitflags;
 use hashbrown::{HashMap, HashSet};
 use lazy_static::lazy_static;
 use serde::{Deserialize, Deserializer, Serialize};
-use serde_json::Value;
+use serde_json::{Map, Value};
 use serde_with::serde_as;
 use serde_with::DefaultOnError;
 
@@ -653,11 +653,8 @@ pub struct SearchFilter {
 #[serde(rename_all = "camelCase", default)]
 pub struct Settings {
     pub general: GeneralSettings,
-    pub shortcuts: Shortcuts,
-    pub meter: MeterTabs,
-    pub logs: LogTabs,
-    pub buffs: BuffSettings,
-    pub sync: SyncSettings,
+    #[serde(flatten)]
+    pub extra: Map<String, Value>
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -711,165 +708,6 @@ pub struct GeneralSettings {
     pub boss_only_damage_default_on: bool,
     pub start_on_boot: bool,
     pub logs_per_page: i32,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", default)]
-pub struct Shortcuts {
-    pub hide_meter: Shortcut,
-    pub show_logs: Shortcut,
-    pub show_latest_encounter: Shortcut,
-    pub reset_session: Shortcut,
-    pub pause_session: Shortcut,
-    pub manual_save: Shortcut,
-    pub disable_clickthrough: Shortcut,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", default)]
-pub struct Shortcut {
-    pub modifier: String,
-    pub key: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", default)]
-pub struct LogTabs {
-    pub abbreviate_header: bool,
-    #[serde(default = "default_true")]
-    pub split_party_damage: bool,
-    #[serde(default = "default_true")]
-    pub split_party_buffs: bool,
-    pub damage: bool,
-    pub dps: bool,
-    pub damage_percent: bool,
-    pub death_time: bool,
-    #[serde(default = "default_true")]
-    pub incapacitated_time: bool,
-    pub crit_rate: bool,
-    pub crit_dmg: bool,
-    pub front_atk: bool,
-    pub back_atk: bool,
-    #[serde(default = "default_true")]
-    pub percent_buff_by_sup: bool,
-    #[serde(default = "default_true")]
-    pub percent_identity_by_sup: bool,
-    #[serde(default = "default_true")]
-    pub percent_brand: bool,
-    #[serde(default = "default_true")]
-    pub percent_hat_by_sup: bool,
-    #[serde(default = "default_true")]
-    pub positional_dmg_percent: bool,
-    pub counters: bool,
-    pub min_encounter_duration: i32,
-    #[serde(default = "default_true")]
-    pub rdps_split_party: bool,
-    #[serde(default = "default_true")]
-    pub rdps_damage_given: bool,
-    #[serde(default = "default_true")]
-    pub rdps_damage_received: bool,
-    #[serde(default = "default_true")]
-    pub rdps_contribution: bool,
-    #[serde(default = "default_true")]
-    pub rdps_s_contribution: bool,
-    #[serde(default = "default_true")]
-    pub rdps_d_contribution: bool,
-    #[serde(default = "default_true")]
-    pub rdps_syn: bool,
-    #[serde(default = "default_true")]
-    pub rdps_s_syn: bool,
-    #[serde(default = "default_true")]
-    pub rdps_d_syn: bool,
-    #[serde(default = "default_true")]
-    pub ssyn: bool,
-    pub breakdown: BreakdownTabs,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", default)]
-pub struct MeterTabs {
-    pub boss_hp: bool,
-    pub boss_hp_bar: bool,
-    pub split_boss_hp_bar: bool,
-    pub abbreviate_header: bool,
-    pub show_time_until_kill: bool,
-    pub show_class_colors: bool,
-    #[serde(default = "default_true")]
-    pub split_party_buffs: bool,
-    #[serde(default = "default_true")]
-    pub pin_self_party: bool,
-    pub damage: bool,
-    pub dps: bool,
-    pub damage_percent: bool,
-    pub death_time: bool,
-    pub incapacitated_time: bool,
-    pub crit_rate: bool,
-    pub crit_dmg: bool,
-    pub front_atk: bool,
-    pub back_atk: bool,
-    pub percent_brand: bool,
-    pub percent_buff_by_sup: bool,
-    pub percent_identity_by_sup: bool,
-    pub percent_hat_by_sup: bool,
-    #[serde(default = "default_true")]
-    pub positional_dmg_percent: bool,
-    pub counters: bool,
-    #[serde(default = "default_true")]
-    pub rdps_split_party: bool,
-    pub rdps_damage_given: bool,
-    pub rdps_damage_received: bool,
-    pub rdps_contribution: bool,
-    pub rdps_s_contribution: bool,
-    pub rdps_d_contribution: bool,
-    #[serde(default = "default_true")]
-    pub rdps_syn: bool,
-    #[serde(default = "default_true")]
-    pub rdps_s_syn: bool,
-    #[serde(default = "default_true")]
-    pub rdps_d_syn: bool,
-    #[serde(default = "default_true")]
-    pub ssyn: bool,
-    pub breakdown: BreakdownTabs,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", default)]
-pub struct BreakdownTabs {
-    pub damage: bool,
-    pub dps: bool,
-    pub damage_percent: bool,
-    pub crit_rate: bool,
-    pub crit_dmg: bool,
-    pub front_atk: bool,
-    pub back_atk: bool,
-    pub percent_buff_by_sup: bool,
-    pub percent_identity_by_sup: bool,
-    pub percent_hat_by_sup: bool,
-    pub percent_brand: bool,
-    pub avg_damage: bool,
-    pub max_damage: bool,
-    pub casts: bool,
-    pub cpm: bool,
-    pub hits: bool,
-    pub hpm: bool,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", default)]
-pub struct BuffSettings {
-    #[serde(default = "default_true")]
-    pub default: bool,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", default)]
-pub struct SyncSettings {
-    pub enabled: bool,
-    pub access_token: String,
-    pub auto: bool,
-    pub username: String,
-    pub valid_token: bool,
-    pub visibility: String,
 }
 
 #[derive(Default, Debug, Serialize)]
