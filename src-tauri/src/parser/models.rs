356b26a1135c3777af278b5e45f26f5b01cc4824
snow <lz@snow.xyz>
2025-04-24 04:06:49 -0400

    add missing fn back

--- src-tauri/src/parser/models.rs
+++ src-tauri/src/parser/models.rs
@@ -992,3 +992,15 @@ pub struct LocalPlayer {
 fn default_true() -> bool {
     true
 }
+
+fn int_or_string_as_string<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = Value::deserialize(deserializer)?;
+    match value {
+        Value::String(s) => Ok(s),
+        Value::Number(n) => Ok(n.to_string()),
+        _ => Err(serde::de::Error::custom("Expected a string or an integer")),
+    }
+}
