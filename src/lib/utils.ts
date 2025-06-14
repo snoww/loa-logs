ec37210d310af99763079c81fed77cd84c924636
William Yu <sinophilic@gmail.com>
2025-06-13 22:57:01 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -82,8 +82,8 @@ export function customRound(num: number, decimalPlaces = 1) {
 export function abbreviateNumber(n: number) {
   if (n >= 1e3 && n < 1e6) return (n / 1e3).toFixed(1) + "k";
   if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "m";
-  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "b";
-  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "t";
+  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(2) + "b";
+  if (n >= 1e12) return +(n / 1e12).toFixed(2) + "t";
   else return tryParseInt(n).toFixed(0);
 }
 
