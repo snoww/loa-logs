975ed56de58d33a365c669672e82fa2621dbb2ec
Dmytro Anyduck <dmytro.anyduck@gmail.com>
2025-08-30 02:40:08 +0300

    chore: remove unused plugin
    They made shell commands async in Tauri v2, so I used `std::process::Command` with hidden flag instead

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -53,7 +53,6 @@ async fn main() -> Result<()> {
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_single_instance::init(|_app, _argv, _cwd| {}))
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
