edc7a13d89c9cade3ad68eb9135b838466eb2c1b
snow <lz@snow.xyz>
2025-11-06 14:46:25 -0500

    unload windivert before launch

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -70,15 +70,15 @@ fn check_updates(app_handle: &AppHandle) -> Arc<AtomicBool> {
         let app_handle = app_handle.clone();
 
         let check_update = async move {
+            // unload driver regardless in case of leftover windivert from other apps?
+            let shell_manager = app_handle.state::<ShellManager>();
+            shell_manager.unload_driver().await;
+
             match app_handle.updater().unwrap().check().await {
                 #[cfg(not(debug_assertions))]
                 Ok(Some(update)) => {
                     info!("update available, downloading update: v{}", update.version);
-
-                    let shell_manager = app_handle.state::<ShellManager>();
-                    shell_manager.unload_driver().await;
                     shell_manager.remove_driver().await;
-
                     if let Err(e) = update.download_and_install(|_, _| {}, || {}).await {
                         error!("failed to download update: {}", e);
                     }
