76182d7b27c146031792e46747d5c626c6a75d0f
snow <lz@snow.xyz>
2025-12-17 13:52:24 -0500

    track certain buffs only at max stacks

--- src-tauri/src/live/entity_tracker.rs
+++ src-tauri/src/live/entity_tracker.rs
@@ -298,8 +298,7 @@ impl EntityTracker {
             if status_effect.status_effect_type == StatusEffectType::Shield {
                 shields.push(status_effect.clone());
             }
-            // hard-code standing strike to not count unless it's at max stacks
-            if status_effect.unique_group == 2000440 && status_effect.stack_count != 6 {
+            if !buff_at_max_stacks(&status_effect) {
                 continue;
             }
             self.status_tracker
@@ -518,8 +517,7 @@ impl EntityTracker {
             source_encounter_entity,
         );
 
-        // hard-code standing strike to not count unless it's at max stacks
-        if status_effect.unique_group == 2000440 && status_effect.stack_count != 6 {
+        if !buff_at_max_stacks(&status_effect) {
             return status_effect;
         }
 
@@ -616,6 +614,16 @@ pub fn get_skill_class_id(skill_id: &u32) -> u32 {
         skill.class_id
     } else {
         0
+    }
+}
+
+// only track certain buffs when they are at max stacks
+pub fn buff_at_max_stacks(status_effect: &StatusEffectDetails) -> bool {
+    match status_effect.unique_group {
+        2000440 => status_effect.stack_count == 6, // standing strike
+        2003220 => status_effect.stack_count == 5, // master
+        2003240 => status_effect.stack_count == 5, // luminary
+        _ => true,
     }
 }
 
