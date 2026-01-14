bf55333b4a4b5da636bd453679b1567a4e0fea1e
snow <lz@snow.xyz>
2026-01-14 07:42:11 -0500

    fix data deserialize

--- src-tauri/src/models/utils.rs
+++ src-tauri/src/models/utils.rs
@@ -12,3 +12,15 @@ where
         _ => Err(serde::de::Error::custom("Expected a string or an integer")),
     }
 }
+
+pub fn int_or_string_as_option_string<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = Value::deserialize(deserializer)?;
+    match value {
+        Value::String(s) => Ok(Some(s)),
+        Value::Number(n) => Ok(Some(n.to_string())),
+        _ => Ok(None),
+    }
+}
