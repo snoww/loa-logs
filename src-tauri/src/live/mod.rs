ea4d9f2d09cd3a0aa186d8a8fe9e08c74bca0090
snow <lz@snow.xyz>
2026-02-06 21:14:32 +0900

    fix machinist ex-zero skill cast tracking

--- src-tauri/src/live/mod.rs
+++ src-tauri/src/live/mod.rs
@@ -402,7 +402,8 @@ pub fn start(args: StartArgs) -> Result<()> {
                     let mut entity = entity_tracker.get_source_entity(pkt.source_id);
                     entity_tracker.guess_is_player(&mut entity, pkt.skill_id);
                     // tracking arcana cards, bard major/minor chords
-                    if entity.class_id == 202 || entity.class_id == 204 {
+                    // machinist transform x skill
+                    if entity.class_id == 202 || entity.class_id == 204 || entity.class_id == 505 {
                         state.on_skill_start(
                             &entity,
                             pkt.skill_id,
