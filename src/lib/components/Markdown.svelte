b8901290ba9fc40ade857e9d8c0ebc8107f565f0
tomaioo <hongsaygio.com@gmail.com>
2026-06-19 23:14:54 -0700

    fix(security): 2 improvements across 2 files
    - Security: Unsafe HTML Rendering in Markdown Component
    - Security: Unsafe HTML Rendering in BuffTooltip Component
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" module>
   import { markdownIt } from "$lib/stores.svelte";
+  import DOMPurify from "dompurify";
 
   export { markdown };
 </script>
@@ -8,6 +9,6 @@
   <div
     class="prose-a:text-accent-500 prose prose-sm prose-zinc prose-invert prose-strong:text-neutrual-200 prose-em:text-neutrual-200 prose-headings:text-neutral-200 prose-img:-my-2 prose-img:rounded-md prose-img:border prose-img:border-neutral-700 prose-img:shadow-md mx-auto max-w-[180rem] px-14 py-6 text-neutral-200"
   >
-    {@html markdownIt.render(body)}
+    {@html DOMPurify.sanitize(markdownIt.render(body))}
   </div>
 {/snippet}
